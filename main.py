4e48b39b863db9072d0b03221c4551468e8336dc
Laura Johnson <lauramjohnson@me.com>
2017-01-30 17:49:18 -0600

    labels and boxes created

---
+++ main.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>UserSignup</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+        label{
+            width: 500;
+        }
+    </style>
+</head>
+<body>
+    <h1>
+        User Signup
+    </h1>
+"""
+
+page_footer = """
+</body>
+</html>
+"""
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        newuser_form = """
+        <form action= "/created" method="post">
+        <label>
+            Username
+            <input type="text" name="new-movie"/>
+        </label>
+        <br>
+        <label>
+            Password
+            <input type="text" name="new-movie"/>
+        </label>
+        <br>
+        <label>
+            Verify Password
+            <input type="text" name="new-movie"/>
+        </label>
+        <br>
+        <label>
+            Email (optional)
+            <input type="text" name="new-movie"/>
+        </label>
+        <br>
+        <input type="submit" value="Create User"/>
+
+        </form>
+        """
+
+
+        self.response.write(page_header + newuser_form + page_footer)
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
